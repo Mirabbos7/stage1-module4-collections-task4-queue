ceae79785fefb507f0f514810aa232c7f2cabe29
Mirabbos <mirabbosahmedov7777@gmail.com>
2024-06-12 22:23:22 +0500

    Queue

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -1,10 +1,20 @@
 package com.epam.collections.queue;
 
-import java.util.ArrayDeque;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> arrayDeque = new ArrayDeque<>();
+        while (!firstQueue.isEmpty() && !secondQueue.isEmpty()) {
+            // Each player adds two cards to the ArrayDeque
+            arrayDeque.add(firstQueue.poll());
+            arrayDeque.add(secondQueue.poll());
+
+            // Then, each player moves one card from their queue to the end
+            firstQueue.add(firstQueue.poll());
+            secondQueue.add(secondQueue.poll());
+        }
+
+        return arrayDeque;
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,34 @@
 package com.epam.collections.queue;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        List<Integer> list = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= numberOfDishes; i++) {
+            queue.offer(i);
+        }
+        while (!queue.isEmpty()) {
+            // Eat everyDishNumberToEat dishes
+            for (int i = 0; i < everyDishNumberToEat; i++) {
+                // If queue is empty, break the loop
+                if (queue.isEmpty()) {
+                    break;
+                }
+                // Eat the next dish
+                int eatenDish = queue.poll();
+                list.add(eatenDish);
+            }
+            // Rotate the queue
+            if (!queue.isEmpty()) {
+                int lastDish = queue.poll();
+                queue.offer(lastDish);
+            }
+        }
+        return list;
     }
 }
