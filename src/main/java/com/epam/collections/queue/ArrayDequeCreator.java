8fbef76c2640da45fe7727a417953569853de16a
Mirabbos <mirabbosahmedov7777@gmail.com>
2024-06-13 13:10:46 +0500

    Commit

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -4,17 +4,33 @@ import java.util.*;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
-        ArrayDeque<Integer> arrayDeque = new ArrayDeque<>();
-        while (!firstQueue.isEmpty() && !secondQueue.isEmpty()) {
-            // Each player adds two cards to the ArrayDeque
-            arrayDeque.add(firstQueue.poll());
-            arrayDeque.add(secondQueue.poll());
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
 
-            // Then, each player moves one card from their queue to the end
-            firstQueue.add(firstQueue.poll());
-            secondQueue.add(secondQueue.poll());
+        if (firstQueue.size() >= 2 && secondQueue.size() >= 2) {
+            deque.add(firstQueue.poll());
+            deque.add(firstQueue.poll());
+            deque.add(secondQueue.poll());
+            deque.add(secondQueue.poll());
         }
 
-        return arrayDeque;
+        while (!firstQueue.isEmpty() || !secondQueue.isEmpty()) {
+            if (!deque.isEmpty()) {
+                firstQueue.add(deque.poll());
+            }
+            if (firstQueue.size() >= 2) {
+                deque.add(firstQueue.poll());
+                deque.add(firstQueue.poll());
+            }
+
+            if (!deque.isEmpty()) {
+                secondQueue.add(deque.poll());
+            }
+            if (secondQueue.size() >= 2) {
+                deque.add(secondQueue.poll());
+                deque.add(secondQueue.poll());
+            }
+        }
+
+        return deque;
     }
 }
